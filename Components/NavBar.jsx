38e32b4c0122ae365979a2b44f66d5a22d551d69
Aswinraj <aswin005achu@gmail.com>
2022-11-11 02:30:41 -0800

    NavBar done as responsive

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
+import profile from '../Pictures/ReactLogo.png'
 
 function NavBar() {
+  const [DarkMode, setdarkMode] = useState(false);
+  let classToggle = DarkMode ? "Dark" : "";
   return (
-    <div className="NavBar">
-      <img src="..."/>
-      <h1>Axvin_Axu</h1>
-      <button>Resume</button>
-    </div>
+    <>
+      <div className={`NavBar${classToggle}`}>
+        <div className="container-fluid">
+          <div className="col-sm-2">
+            <img src={profile} className="mt-4 ml-0" />
+            <h1 className='title pl-5 pt-4 ml-5'>Axvin_Axu</h1>
+          </div>
+          <div className="col-9 container">
+            <button className='resume'>Resume</button>
+          </div>
+          <div className="col-11 container-fluid">
+            <label className='switch'>
+              <input type="checkbox" value={DarkMode} onClick={(e) => {
+                setdarkMode(e.target.checked);
+              }} />
+              <span className='slider' />
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
